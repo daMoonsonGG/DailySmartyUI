a4f8362f1eb64f79a720ce63c62f91e2c3ff030d
Alexandre <alonsocabado@hotmail.com>
2021-02-22 10:55:30 +0100

    Setup logo component and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        logo
+      <div className="app-wrapper">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="search-bar">
+          <input placeholder="DailySmarty" />
+        </div>
+        <div className="recent-posts">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img
+          alt="daily smarty ui image logo"
+          src="/assets/ds_circle_logo.png"
+        />
+      </div>
+    );
+  }
+}
