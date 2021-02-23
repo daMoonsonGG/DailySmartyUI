5e87b20967982218657f3bc78f93ceebec47608a
Alexandre <alonsocabado@hotmail.com>
2021-02-23 14:06:33 +0100

    Mapped state to props in results posts component and rendered each post component, and implemented home link in logo component.

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 export default class Logo extends Component {
   render() {
     const size = {
@@ -8,11 +10,13 @@ export default class Logo extends Component {
     };
     return (
       <div className="logo-main">
-        <img
-          style={size}
-          alt="daily smarty ui image logo"
-          src="/assets/ds_circle_logo.png"
-        />
+        <Link to="/">
+          <img
+            style={size}
+            alt="daily smarty ui image logo"
+            src="/assets/ds_circle_logo.png"
+          />
+        </Link>
       </div>
     );
   }
--- src/components/resultsPosts.js
+++ src/components/resultsPosts.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
+import Post from "./post";
+
 import { connect } from "react-redux";
 
 class ResultsPosts extends Component {
+  renderPosts() {
+    const posts = this.props.posts.map((post, index) => {
+      return <Post key={index} {...post} />;
+    });
+    return posts;
+  }
   render() {
     return (
       <div className="results-posts">
         <div className="results-posts__wrapper">
-          <ul className="results-posts__posts">RESULTS GO HERE</ul>
+          <ul className="results-posts__posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
@@ -15,7 +23,7 @@ class ResultsPosts extends Component {
 }
 
 function mapStateToProps(state) {
-  return { state };
+  return { posts: state.posts.resultsPosts };
 }
 
 export default connect(mapStateToProps)(ResultsPosts);
